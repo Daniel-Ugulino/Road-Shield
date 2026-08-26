6373bf634e57016435635f53817f0d2b92520180
Daniel-Ugulino <danielugulino46@gmail.com>
2026-08-26 18:22:13 -0300

    feat: ajuste no modulo de controle

--- assembly/control.s
+++ assembly/control.s
@@ -10,93 +10,71 @@
 .extern cfg_vel_max
 
 control:
-    cmp     w0, #'A'
-    b.lt    control_no_upper
-    cmp     w0, #'Z'
-    b.gt    control_no_upper
-    add     w0, w0, #('a' - 'A')
-control_no_upper:
-    cmp w0, #'w'
-    beq increase_max_speed
-    cmp w0, #'s'
-    beq reduce_max_speed
-    cmp w0, #'a'
-    beq increase_risk_zone
-    cmp w0, #'d'
-    beq reduce_risk_zone
+    stp     x29, x30, [sp, #-32]!
+    mov     x29, sp
+    str     x19, [sp, #16]
 
-    mov w0, #0
-    ret
+    orr     w0, w0, #0x20
+
+    cmp     w0, #'w'
+    b.eq    increase_max_speed
+    cmp     w0, #'s'
+    b.eq    reduce_max_speed
+    cmp     w0, #'a'
+    b.eq    increase_risk_zone
+    cmp     w0, #'d'
+    b.eq    reduce_risk_zone
+
+    mov     w0, #0
+    b       control_ret
 
 increase_max_speed:
-    adr     x9, cfg_vel_max
-    ldrb    w10, [x9]
-    add     w10, w10, #STEP_VEL
-    cmp     w10, #255
-    b.le    increase_max_speed_store
-    mov     w10, #255
-increase_max_speed_store:
-    strb    w10, [x9]
-    mov     w0, #1
-    bl      log_control_action
-    mov     w0, #0x01
-    ret
+    mov     w1, #STEP_VEL
+    mov     w19, #0x01
+    b       apply_speed
 
 reduce_max_speed:
+    mov     w1, #-STEP_VEL
+    mov     w19, #0x02
+
+apply_speed:
     adr     x9, cfg_vel_max
-    ldrb    w10, [x9]
-    subs    w10, w10, #STEP_VEL
-    b.hs    reduce_max_speed_store
-    mov     w10, #0
-reduce_max_speed_store:
-    strb    w10, [x9]
-    mov     w0, #2
-    bl      log_control_action
-    mov     w0, #0x02
-    ret
+    bl      adjust_byte
+    b       control_log
 
 increase_risk_zone:
-    adr     x9, cfg_dist_free
-    adr     x10, cfg_dist_att
-    ldrb    w11, [x9]
-    ldrb    w12, [x10]
-    add     w11, w11, #STEP_DIST
-    add     w12, w12, #STEP_DIST
-    cmp     w11, #255
-    b.le    increase_risk_zone_check_att
-    mov     w11, #255
-increase_risk_zone_check_att:
-    cmp     w12, w11
-    b.le    increase_risk_zone_store
-    mov     w12, w11
-increase_risk_zone_store:
-    strb    w11, [x9]
-    strb    w12, [x10]
-    mov     w0, #3
-    bl      log_control_action
-    mov     w0, #0x03
-    ret
+    mov     w1, #STEP_DIST
+    mov     w19, #0x03
+    b       apply_risk_zone
 
 reduce_risk_zone:
+    mov     w1, #-STEP_DIST
+    mov     w19, #0x04
+
+apply_risk_zone:
     adr     x9, cfg_dist_free
-    adr     x10, cfg_dist_att
-    ldrb    w11, [x9]
-    ldrb    w12, [x10]
-    subs    w11, w11, #STEP_DIST
-    b.hs    reduce_risk_zone_sub_att
-    mov     w11, #0
-reduce_risk_zone_sub_att:
-    subs    w12, w12, #STEP_DIST
-    b.hs    reduce_risk_zone_store
-    mov     w12, #0
-reduce_risk_zone_store:
-    cmp     w12, w11
-    b.le    reduce_risk_zone_write
-    mov     w12, w11
-reduce_risk_zone_write:
-    strb    w11, [x9]
-    strb    w12, [x10]
-    mov     w0, #4
+    bl      adjust_byte
+    adr     x9, cfg_dist_att
+    bl      adjust_byte
+
+control_log:
+    mov     w0, w19
     bl      log_control_action
-    mov     w0, #0x04
+    mov     w0, w19
+
+control_ret:
+    ldr     x19, [sp, #16]
+    ldp     x29, x30, [sp], #32
+    ret
+
+// [x9] += w1, saturando em 0..255
+adjust_byte:
+    ldrb    w2, [x9]
+    add     w2, w2, w1
+    cmp     w2, #255
+    mov     w3, #255
+    csel    w2, w2, w3, le
+    cmp     w2, #0
+    csel    w2, w2, wzr, ge
+    strb    w2, [x9]
     ret
